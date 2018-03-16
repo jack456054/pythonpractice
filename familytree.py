7ae0f3287b33a9272c4369ef451226e4b23d0527
WenYu Lee <leewen@oregonstate.edu>
2018-03-15 20:43:54 -0700

    Familytree

---
+++ familytree.py
@@ -0,0 +1,36 @@
+
+
+def family_tree(namelist):
+    name = []
+    for _, ((dad, mom), child) in enumerate(namelist):
+        if not name:
+            name.append([dad, mom])
+            name.append([child])
+        else:
+            for index in range(len(name)):
+                if child in name[index]:
+                    name.insert(index, [dad, mom])
+                    break
+                elif mom in name[index]:
+                    if index == len(name) - 1:
+                        name.append([child])
+                    else:
+                        name[index + 1].append(child)
+                    if dad not in name[index]:
+                        name[index].append(dad)
+                    break
+
+                elif dad in name[index]:
+                    if index == len(name) - 1:
+                        name.append([child])
+                    else:
+                        name[index + 1].append(child)
+                    if mom not in name[index]:
+                        name[index].append(mom)
+                    break
+
+        return name
+
+
+if __name__ == '__main__':
+    family_tree([(['A', 'C'], 'B'), (['B', 'D'], 'E'), (['D', 'F'], 'G')])
