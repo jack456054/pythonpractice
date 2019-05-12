71eb89c3a0869079cc830a3dcf9ef13bd889c39f
jack456054 <jack456054@hotmail.com>
2019-05-11 23:03:36 -0700

    Decorator example

--- decorator_practice.py
+++ decorator_practice.py
@@ -6,7 +6,7 @@ def my_decorator(func):
     def function_that_runs_func():
         print('In the decorator.')
         func()
-        print('After thr decorator.')
+        print('After the decorator.')
     return function_that_runs_func
 
 
@@ -15,5 +15,25 @@ def say_hello():
     print('Hello!')
 
 
+def decorator_with_arguments(number):
+    def my_decorator(func):
+        @functools.wraps(func)
+        def function_that_runs_func(*args, **kwargs):
+            print('In the decorator.')
+            if number == 56:
+                print('Not running the func')
+            else:
+                func(*args, **kwargs)
+            print('After the decorator.')
+        return function_that_runs_func
+    return my_decorator
+
+
+@decorator_with_arguments(57)
+def say_hello_two(a, b):
+    print(a * b)
+
+
 if __name__ == '__main__':
     say_hello()
+    say_hello_two(1, 2)
